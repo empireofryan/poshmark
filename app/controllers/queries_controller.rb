ce1b293177c4890f9ac5415b011a70ba80492e93
empireofryan <rjohnson2001@gmail.com>
2017-04-08 02:42:51 -0400

    returns results correctly, html and spaces in query

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -23,8 +23,18 @@ class QueriesController < ApplicationController
       req.on_complete do |resp|
         if resp.success?
           responses << resp.body
-        #  puts responses
-           puts 'da shiz werks'
+          # puts responses
+          puts 'responses size'
+          puts responses.size
+          puts 'responses to_s'
+          a = responses.to_s
+          puts a.class
+          puts 'da shiz werks'
+          dapage = Nokogiri::HTML(a)
+          puts dapage
+          yoyo = dapage.css('.tile')
+          c = yoyo.count.to_i
+          #puts c
         else
           responses << "#{url} failed with #{resp.code}"
           puts 'error ryan robinson!'
@@ -35,6 +45,7 @@ class QueriesController < ApplicationController
     #end
 
   hydra.run # let the magic begin!
+
   byebug
     redirect_to items_path
   end
