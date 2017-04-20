5eac005a79afa05b19bb1f225198d4d931f39bf5
empireofryan <rjohnson2001@gmail.com>
2017-04-20 00:40:51 -0400

    spectrum showing

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,4 +1,19 @@
 class Query < ApplicationRecord
   has_many :items, inverse_of: :query
   accepts_nested_attributes_for :items
+
+  def min
+    @item_query = Item.where(query_id: Item.last.query_id)
+    @item_query.minimum(:price).to_i
+  end
+
+  def max
+    @item_query = Item.where(query_id: Item.last.query_id)
+    @item_query.maximum(:price).to_i
+  end
+
+  def spectrum(num)
+    num / (min + max)
+  end
+
 end
